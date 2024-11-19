9b82ef223cf8bde0a82ecc09306b1ff3cacd96c8
Subhankar <guchaitsubhankar0@gmail.com>
2024-11-19 23:05:45 +0530

    save in file

--- ex8/models/product.js
+++ ex8/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../util/path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +9,33 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, "data", "products.json"); //path for save products in file
+
+    //reading file
+    fs.readFile(p, (err, data) => {
+      //note: If we don't use arrow function it will not work because in arrow function "this" keyworsd will refer to their surrounding lexical context(ex: here product) but in normal function "this" will refer to global in non strict mode and in strict mode undefined
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      }
+
+      products.push(this); // here this will refer to current product
+
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(data));
+    });
   }
 };
--- ex8/routes/shop.js
+++ ex8/routes/shop.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const productController = require("../controllers/products");
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get("/", productController.getProduct);
+router.get('/', productsController.getProducts);
 
 module.exports = router;
