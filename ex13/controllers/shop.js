8be5f23a1f866be24c079b521c644463666da03d
Subhankar <guchaitsubhankar0@gmail.com>
2024-11-22 21:33:56 +0530

    retrieve data from database and delete data from database

---
+++ ex13/controllers/shop.js
@@ -0,0 +1,70 @@
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows]) => {
+      console.log(rows);
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(([product]) => {
+      // console.log(product);
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
